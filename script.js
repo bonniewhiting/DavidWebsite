a6d9670bda53afcb15457097f8910f6b51929ff1
Mason <masonlynass@gmail.com>
2023-08-29 22:05:17 -0700

    two new stories, mobile css, still need to update the nav with new stories

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ button.addEventListener('click', function(){
     menuUL.classList.remove('is-active')
   } else menuUL.classList.add('is-active');
 });
+
+const mobileScreen = window.matchMedia('(max-width: 600px')
+const nav = document.getElementsByClassName('nav')[0]
+const navStories = document.getElementById('nav-stories');
+const navReviews = document.getElementById('nav-reviews');
+
+if (mobileScreen.matches) {
+
+  let stories = document.createElement('li')
+  let reviews = document.createElement('li')
+  
+  let storiesLink = document.createElement('a')
+  storiesLink.classList.add('otherPage')
+  storiesLink.setAttribute('href', '/stories.html')
+  storiesLink.innerText = "Stories"
+
+  let reviewsLink = document.createElement('a')
+  reviewsLink.classList.add('otherPage')
+  reviewsLink.setAttribute('href', '/reviews.html')
+  reviewsLink.innerText = 'Reviews'
+
+  stories.append(storiesLink)
+  reviews.append(reviewsLink)
+
+  navStories.replaceWith(stories)
+  navReviews.replaceWith(reviews)
+}
